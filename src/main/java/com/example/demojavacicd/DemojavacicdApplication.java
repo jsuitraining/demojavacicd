42199d38a9cbfe03fe854ceb46262037056e0863
js <js@jss-iMac.local>
2023-08-26 07:43:53 +0530

    added java files

---
+++ src/main/java/com/example/demojavacicd/DemojavacicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojavacicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojavacicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojavacicdApplication.class, args);
+	}
+
+}
